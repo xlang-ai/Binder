00b3a1a85d57f370c1caa1aa55b2e95c27d633ca
Timothyxxx <384084775@qq.com>
2022-10-05 17:01:34 +0800

    Add commands for running

---
+++ run.py
@@ -0,0 +1,57 @@
+import os
+
+ROOT_DIR = os.path.join(os.path.dirname(__file__))
+
+# Disable the TOKENIZERS_PARALLELISM
+TOKENIZER_FALSE = "export TOKENIZERS_PARALLELISM=false\n"
+
+# wikitq nsql annotation command
+os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/annotate_binder_program.py --dataset wikitq \
+--dataset_split test \
+--prompt_file templates/prompts/prompt_wikitq_v3.txt \
+--n_parallel_prompts 1 \
+--max_generation_tokens 512 \
+--temperature 0.4 \
+--sampling_n 20 \
+-v""")
+
+# wikitq nsql execution command
+os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/execute_binder_program.py --dataset wikitq \
+--dataset_split test \
+--qa_retrieve_pool_file templates/prompts/qa_retrieve_pool.txt \
+--input_program_file binder_program_wikitq_test.json \
+--output_program_execution_file binder_program_wikitq_test_exec.json \
+--vote_method simple""")
+
+# tab_fact nsql annotation command
+# os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/annotate_binder_program.py --dataset tab_fact \
+# --dataset_split test \
+# --prompt_file templates/prompts/prompt_tab_fact_sqllike_v3.txt \
+# --n_parallel_prompts 1 \
+# --max_generation_tokens 512 \
+# --temperature 0.6 \
+# --sampling_n 60 \
+# -v""")
+
+# tab_fact nsql execution command
+# os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/execute_binder_program.py --dataset tab_fact \
+# --dataset_split test \
+# --qa_retrieve_pool_file templates/prompts/qa_retrieve_pool.txt \
+# --input_program_file binder_program_tab_fact_test.json \
+# --output_program_execution_file binder_program_tab_fact_test_exec.json \
+# --vote_method answer_biased \
+# --answer_biased 1 \
+# --answer_biased_weight 4 \
+# """)
+
+# More
+# tab_fact nsql annotation command
+
+# mmqa nsql annotation command
+
+# mmqa nsql execution command
+
+# Analysis(Still working on cleaning these code)
+# scalability command
+# robustness command
+# python command
