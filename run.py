1b08838cb3f420a6dd0bfb3b4743ed92d281a059
Timothyxxx <384084775@qq.com>
2022-10-07 00:50:15 +0800

    Update shell to run

--- run.py
+++ run.py
@@ -16,21 +16,24 @@ TOKENIZER_FALSE = "export TOKENIZERS_PARALLELISM=false\n"
 # -v""")
 
 # wikitq nsql execution command
-os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/execute_binder_program.py --dataset wikitq \
---dataset_split test \
---qa_retrieve_pool_file templates/qa_retrieve_pool.json \
---input_program_file binder_program_wikitq_test.json \
---output_program_execution_file binder_program_wikitq_test_exec.json \
---vote_method simple""")
+# os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/execute_binder_program.py --dataset wikitq \
+# --dataset_split test \
+# --qa_retrieve_pool_file templates/qa_retrieve_pool.json \
+# --input_program_file binder_program_wikitq_test.json \
+# --output_program_execution_file binder_program_wikitq_test_exec.json \
+# --vote_method simple
+# """)
 
 # tab_fact nsql annotation command
 os.system(fr"""{TOKENIZER_FALSE}python {ROOT_DIR}/scripts/annotate_binder_program.py --dataset tab_fact \
 --dataset_split test \
---prompt_file templates/prompt_tab_fact_sqllike_v3.txt \
+--prompt_file templates/prompts/prompt_tab_fact_sqllike_v3.txt \
 --n_parallel_prompts 1 \
---max_generation_tokens 512 \
+--n_processes 1 \
+--n_shots 18 \
+--max_generation_tokens 256 \
 --temperature 0.6 \
---sampling_n 60 \
+--sampling_n 50 \
 -v""")
 
 # tab_fact nsql execution command
