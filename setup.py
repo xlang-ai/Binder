9af1bf4573d6559cfe37b26a4b9e9aff209086b9
Yiheng Xu <charlesyihengxu@gmail.com>
2022-11-11 16:33:50 +0800

    package binder

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+REQUIRES_PYTHON = ">=3.7.0"
+NAME = "binder"
+
+install_requires = [
+    "emoji==1.7.0",
+    "fuzzywuzzy==0.18.0",
+    "nltk==3.6.2",
+    "sqlparse==0.4.2",
+    "recognizers-text-suite==1.0.2a2",
+    "records"
+]
+
+setup(
+    name=NAME,
+    version="0.0.1",
+    python_requires=REQUIRES_PYTHON,
+    packages=find_packages(exclude=("tests*")),
+    install_requires=install_requires,
+)
